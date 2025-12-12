257ad1b8ae9597fcad356e3c905113e485ea7873
Shripad13 <stmaddi@outlook.com>
2025-12-12 12:24:21 +0530

    updated the file

---
+++ securityGroup.tf
@@ -0,0 +1,22 @@
+# This DB has to be accessed only by default vpc & by the workloads running on EKS.
+
+resource "aws_security_group" "main" {
+  name        = "${var.env}-rds-sg"
+  description = "${var.env}-rds-sg"
+
+
+  ingress {
+    description = "Allows RDP"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.eks_subnet_cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 means all protocols TCP & UDP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+# You are declaring the empty varibale bcoz you need to supply from backend module
+
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+
+variable "env" {}
+variable "family" {}
+variable "subnet_ids" {}
+
+variable "eks_subnet_cidr" {}
